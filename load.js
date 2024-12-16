a46f7b1915afaacf20f99e0b8f70d6c51545ea48
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2024-12-16 15:49:24 -0800

    add duration calculation

--- load.js
+++ load.js
@@ -1,3 +1,35 @@
+const getDurationString = (start, end) => {
+    const timeDiff = new Date(end - start);
+    let yearDiff = timeDiff.getUTCFullYear() - 1970;
+    let monthDiff = timeDiff.getUTCMonth() + 1;
+
+    if (monthDiff == 12) {
+        yearDiff++;
+        monthDiff = 0;
+    }
+
+    let toReturn;
+    if (yearDiff == 0) {
+        toReturn = "";
+    } else if (yearDiff == 1) {
+        toReturn = `1 y`;
+    } else {
+        toReturn = `${yearDiff} y`
+    }
+
+    if (yearDiff && monthDiff) {
+        toReturn += ", "
+    }
+
+    if (monthDiff == 1) {
+        toReturn += "1 mo"
+    } else if (monthDiff > 1) {
+        toReturn += `${monthDiff} mo`
+    }
+
+    return toReturn;
+};
+
 const loadWork = async () => {
     const response = await fetch("./res/work.json");
     const workList = await response.json();
@@ -8,12 +40,16 @@ const loadWork = async () => {
         let card = document
             .getElementById("general-card-template")
             .content.cloneNode(true);
-        
-        card.querySelector(".title").textContent = work.title
-        card.querySelector(".date").textContent = new Date(work.from)
-        
-        workExperience.appendChild(card)
 
+        card.querySelector(".title").textContent = work.title;
+        const dateStart = new Date(work.from);
+        const dateEnd = work.to ? new Date(work.to) : new Date();
+        card.querySelector(".date").textContent = `${work.from} \u2012 ${work.to} (${getDurationString(
+            dateStart,
+            dateEnd
+        )})`;
+
+        workExperience.appendChild(card);
     }
 };
 
