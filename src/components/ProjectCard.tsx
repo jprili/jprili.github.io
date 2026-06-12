0aed9c17ae57448814655126947c24082621d27f
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-06-12 16:15:00 -0700

    remove debug

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,6 @@ import type { Project } from "../data/Project";
 import { Tags } from "../components/Tags";
 
 const ProjectCard = (project: Project) => {
-    console.debug(project);
     return (
         <div className="p-card" key={project.key}>
             <div className="p-title">
