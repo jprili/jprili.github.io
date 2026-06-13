a75c2bb17b7d92d3623d35686433763048e5014d
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-06-13 14:53:12 -0700

    add socials placeholder

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = () => {
     return (
         <footer>
-            <p>&#169; { new Date().getFullYear() } James Paolo Rili  </p>
+            <p>&#169; { new Date().getFullYear() } James Paolo Rili</p>
+            <div id="socials">
+                <a href=""><FontAwesomeIcon className="i-link" icon={faLinkedin}/></a>
+                <a href=""><FontAwesomeIcon className="i-link" icon={faGithub}/></a>
+            </div>
         </footer>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
+import portrait from "../../res/portrait.jpg";
+
 const Home = () => {
     return (
-        <div className="content">
-            <h1>Hello</h1>
+        <div className="content home">
+            <h1>Hi, I'm James!</h1>
+            <div className="intro-body">
+            <img className="portrait" src={portrait}/>
+            <p>
+                I'm an Aspiring Software Engineer and Physicist
+                in Vancouver, BC.
+            </p>
+            </div>
         </div>
     );
 }
