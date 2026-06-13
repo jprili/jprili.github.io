0200662e151edc4dbf3b7eb3fc850c8aa29b7524
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-06-13 14:57:43 -0700

    add footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,12 @@ const Footer = () => {
         <footer>
             <p>&#169; { new Date().getFullYear() } James Paolo Rili</p>
             <div id="socials">
-                <a href=""><FontAwesomeIcon className="i-link" icon={faLinkedin}/></a>
-                <a href=""><FontAwesomeIcon className="i-link" icon={faGithub}/></a>
+                <a href="https://linkedin.com/in/jprili">
+                    <FontAwesomeIcon className="i-link" icon={faLinkedin}/>
+                </a>
+                <a href="https://github.com/jprili">
+                    <FontAwesomeIcon className="i-link" icon={faGithub}/>
+                </a>
             </div>
         </footer>
     );
