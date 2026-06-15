c3260739e74485988d611c14e899a9a2c68ac2be
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-06-15 13:10:54 -0700

    fix header bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,19 @@ const Home = () => {
             <p>
                 I'm an Aspiring Software Engineer and Physicist
                 in Vancouver, BC.
+
+                Currently, I am a student in BCIT Computer 
+                Systems Technology program until the end of
+                2026. In the future, I would like to be
+                involved in computational physics and
+                low-level programming.
+
+                I like to play puzzle games in my spare time.
+                Right now, <i>Shenzhen I/O</i> from Zachtronics 
+
+                Feel free to browse around my website,
+                and don't hesitate to contact me if there 
+                are any questions
             </p>
             </div>
         </div>
