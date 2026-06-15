2082390783663f39f7aa8bfc5b8cc2b55c17d742
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-06-15 11:03:40 -0700

    remove mjs file

--- rsbuild.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-
-export default defineConfig({
-    html: {
-        title: "James Paolo Rili"
-    },
-    plugins: [pluginReact()],
-});
